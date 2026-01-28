ec42727dc7b82440f5fcef014e0bb897554b168f
HUngerBalls <diogortcunha@gmail.com>
2026-01-28 20:55:44 +0000

    Fix writeup

--- src/data/writeups.js
+++ src/data/writeups.js
@@ -1,5 +1,3 @@
-const writeupFiles = [
-  `${process.env.PUBLIC_URL}/writeups/pickle-rick/pickle-rick.md`,
-];
+const writeupFiles = ['writeups/pickle-rick/pickle-rick.md'];
 
 export default writeupFiles;
